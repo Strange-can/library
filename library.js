d1745b2688551763b3ed5e2ac21da231e46015c2
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-10-18 23:02:09 +0800

    Add form with html dialog to add new book
    A dialog will now pop up to allow users to add a new book when they press the "NEW BOOK" button.

--- library.js
+++ library.js
@@ -4,69 +4,6 @@ const myLibrary = [ {
     pages: 100,
     status: "has been read"
     },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
-
-    {
-        title: "Lincoln is da legend",
-        author: "Lincoln",
-        pages: 100,
-        status: "has been read"
-    },
 ]
 
 function Book(title, author, pages, readStatus) {
@@ -76,32 +13,68 @@ function Book(title, author, pages, readStatus) {
     this.status = readStatus
 }
 
-Book.prototype.description = function () {
-    console.log(`${this.title} is by ${this.author} that is ${this.pages} pages long, it ${this.status}.`)
-}
-
 let title = ""
 let author = ""
 let pages = ""
-let readStatus = ""
+let readStatus = "reading"
 
 
 function addBookToLibrary() {
-    let getBook = new Book( title, author, pages, readStatus )
-    getBook.description()
+   let getBook = new Book( title, author, pages, readStatus )
    myLibrary.push(getBook)
    getBook = ""
 }
 
-/*const bookTitle = document.querySelector(".title")
-bookTitle.textContent = `Book title: ${myLibrary[0].title}`
-const bookAuthor = document.querySelector(".author")
-bookAuthor.textContent = `Author: ${myLibrary[0].author}`
-const bookPages = document.querySelector(".pages")
-bookPages.textContent = `Pages: ${myLibrary[0].pages}`
-const bookStatus = document.querySelector(".status")
-bookStatus.textContent = `Status: ${myLibrary[0].status}`*/
+const dialog = document.querySelector("dialog")
 
+const showButton = document.getElementById("new-book")
+showButton.addEventListener( "click",  () => {
+    dialog.showModal()
+})
+
+const cancelButton = document.getElementById("cancel")
+cancelButton.addEventListener( "click", () => {
+    form.reset()
+    msg.textContent = ''
+    dialog.close()
+})
+
+const form = document.querySelector("form")
+
+const msg = document.getElementById("kind-msg")
+ 
+const formTitle = document.getElementById("title")
+
+const formAuthor = document.getElementById("author")
+
+const formPages = document.getElementById("pages")
+
+const confirmButton = document.getElementById("confirm") 
+confirmButton.addEventListener( "click", (event) => {
+    event.preventDefault()
+    if ( formTitle.value && formAuthor.value && Number(formPages.value) > 0) {
+    title = formTitle.value
+    author = formAuthor.value
+    pages = formPages.value
+    msg.textContent = ''
+    form.reset()
+    dialog.close()
+    addBookToLibrary()
+    displayCards()
+    title = ''
+    author = ''
+    pages = ''
+    }
+    else if ( formTitle.value && formAuthor.value && !(Number(formPages.value) > 0) ) {
+        msg.textContent = `Did you pull that book outta your ass? Ain\'t no book having ${Number(formPages.value)} pages`
+    }
+    else {
+        msg.textContent = 'Please fill in all fields'
+    }
+})
+
+function displayCards() {
+    //cardContainer.textContent = ''
 for ( let i=0; i < myLibrary.length; i++ ) {
     // make the cards 
     const cardContainer = document.querySelector(".card-container")
@@ -123,4 +96,4 @@ for ( let i=0; i < myLibrary.length; i++ ) {
     bookStatus.textContent = `Status: ${myLibrary[i].status}`
     bookStatus.style.color = "green"
 }
-
+}
